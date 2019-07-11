41a9c322ab2fdb0cea0f30ce38f1ae8592fb6065
Etienne Dubeau <etienne.dubeau.1@ulaval.ca>
2019-07-11 17:36:33 -0400

    Update setup.py to include deeptrack package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='6DOF_tracking_evaluation',
     version='0.1dev',
-    packages=find_packages(),
+    packages=find_packages() + ['ulaval_6dof_object_tracking.deeptrack'],
     author='Mathieu Garon',
     author_email='mathieugaron91@gmail.com',
     requires=['numpy', 'Pillow', 'plyfile', 'pyopengl', 'vispy', 'seaborn', 'pandas']
